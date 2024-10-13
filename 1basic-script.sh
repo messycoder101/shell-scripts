e9dd90cc5e7b1cd36151fa08277727e543a6ee8b
Anurag Prakash <52032738+messycoder101@users.noreply.github.com>
2024-10-13 08:13:35 +0300

    Create 1basic-script.sh

---
+++ 1basic-script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+<< note This is a
+multiline
+note
+
+echo -e "Gabbar: Kitne aadmi the!!??\n"
+
+echo -e "Kaalia: 2, Sarkaar!\n"
+
+echo -e "Gabbar: Kitne baje hai:"
+date
+
+echo -e "\nGabbarL Kitne baje se server chal raha hai?"
+
+uptime
+
+echo -e "\nIs londe ka kya naam hai?\n"
+read naam
+echo -e "\n Swaagat hai, $naam bhaai!! \n"
